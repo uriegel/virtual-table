4a0a4be5b440ff8255579a7a4e20c11135754878
Uwe Riegel <uriegel@hotmail.de>
2023-02-03 19:42:44 +0100

    Scrollbar pageup, pagedown

--- src/component/Scrollbar.tsx
+++ src/component/Scrollbar.tsx
@@ -17,12 +17,45 @@ export const Scrollbar = ({ count, displayCount, headerHeight, scrollbarHeight,
     const gripHeight = Math.max(scrollbarHeight * (displayCount / count || 1), minScrollbarGripSize)
     const scrollbarGripTop = (scrollbarHeight - gripHeight) * (scrollPosition / range) 
 
+    const getGripTop = () => 
+        (scrollbarHeight - gripHeight) * (scrollPosition / (range - 1))
+
+    const mouseRepeat = (action: (pos: number)=>void) => {
+        action(scrollPosition)
+        let interval = 0
+        const timeout = setTimeout(() => interval = setInterval(action, 50), 600)
+        const mouseUp = () => {
+            window.removeEventListener("mouseup", mouseUp)
+            clearTimeout(timeout)
+            if (interval)
+                clearInterval(interval)
+        }
+        window.addEventListener("mouseup", mouseUp)
+    }
+
+    const onPageMouseDown = (revt: React.MouseEvent) => {
+		let newPosition = scrollPosition
+		const evt = revt.nativeEvent
+		const isUp = evt.offsetY <= getGripTop()
+		const action = isUp 
+			? () => {
+				const gripTop = getGripTop()
+				if (evt.offsetY < gripTop)
+					newPosition -= displayCount - 1
+                setScrollPosition(Math.max(newPosition, 0))
+			}
+			: () => {
+				const gripTop = getGripTop()
+				if (evt.offsetY > gripTop + gripHeight)
+					newPosition += displayCount - 1
+                setScrollPosition(Math.min(range -1, newPosition))
+			}
+		mouseRepeat(action)
+	}
+
     const onGripDown = (revt: React.MouseEvent) => {
 		const evt = revt.nativeEvent
         const pixelRange = scrollbarHeight - gripHeight + 1
-
-        const getGripTop = () => 
-            (scrollbarHeight - gripHeight) * (scrollPosition / (range - 1))
 
 		const gripTop = getGripTop()
         const startPos = evt.y - gripTop
@@ -50,7 +83,8 @@ export const Scrollbar = ({ count, displayCount, headerHeight, scrollbarHeight,
     return (
         <div 
             className={`vtr--scrollbar ${count < displayCount ? 'hidden' : ''}`}
-            style={{ top: `${headerHeight}px`, height: `calc(100% - ${headerHeight}px)` }}>
+            style={{ top: `${headerHeight}px`, height: `calc(100% - ${headerHeight}px)` }}
+            onMouseDown={onPageMouseDown} >
             <div className={"vtr--scrollbar-grid"}
                 style={{ top: `${scrollbarGripTop}px`, height: `${gripHeight}px` }}
                 onMouseDown={onGripDown}>
